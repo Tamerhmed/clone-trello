c175591c9ba1c6d24ebfd2174b4a2b96ffa0cc2d
Tamerhmed <tamer30230@gmail.com>
2022-09-17 10:40:29 +1000

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import data from './data';
+
+
+ const App = () => {
+
+     return (
+          <div className='App'>
+              app
+           </div>         
+          
+     );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+const data = [
+      {
+          title: 'group 1',
+          items: ['1', '2', '3']  
+      },
+      {
+          title: 'group 2',
+          items: ['4', '5']  
+      }
+]
+
+export default data;
